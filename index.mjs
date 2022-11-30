79f0bef23193c357533a59a654e831b79d75ae27
Gabriel McGoldrick <gmcgoldr@redhat.com>
2022-11-30 16:15:49 +0000

    init commit

---
+++ index.mjs
@@ -0,0 +1,30 @@
+import remarkEmbedder from '@remark-embedder/core'
+import oembedTransformer from '@remark-embedder/transformer-oembed'
+// or, if you're using CommonJS require:
+// const {default: oembedTransformer} = require('@remark-embedder/transformer-oembed')
+import {remark} from 'remark'
+import html from 'remark-html'
+
+const exampleMarkdown = `
+# My favorite YouTube video
+
+[This](https://www.youtube.com/watch?v=dQw4w9WgXcQ) is a great YouTube video.
+Watch it here:
+
+https://www.youtube.com/watch?v=dQw4w9WgXcQ
+
+Isn't it great!?
+`
+
+async function go() {
+  const result = await remark()
+    .use(remarkEmbedder, {
+      transformers: [oembedTransformer],
+    })
+    .use(html)
+    .process(exampleMarkdown)
+
+  console.log(result.toString())
+}
+
+go()
